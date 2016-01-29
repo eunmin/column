23182fc78205b1767fd527cb43c92f947bcef605
eunmin <telepopsound@gmail.com>
2016-01-29 23:22:36 +0900

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to column
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/column/core_test.clj
@@ -0,0 +1,7 @@
+(ns column.core-test
+  (:require [clojure.test :refer :all]
+            [column.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
